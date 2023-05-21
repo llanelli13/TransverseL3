805918d52652ff63b501f5c36c799a89d7463c89
ElisaChzd <elisa.chazard@efrei.net>
2023-05-21 12:38:23 +0200

    css

--- Screens/ContactUs.js
+++ Screens/ContactUs.js
@@ -4,23 +4,35 @@ import AdminCheck from "../Components/AdminCheck";
 
 const ContactUs = () => {
   return (
-    <View>
-      <Text>Olala contactez nous ....</Text>
+    <View style = {styles.background}>
+      <View style = {styles.container}>
+        <Text>L'équipe BDS</Text>
+        <Image source={require("../Images/Logo.png")} style={styles.logo} />
+        <Text>L'équipe BDS</Text>
+        <Text>L'équipe BDS</Text>
+        <Text>L'équipe BDS</Text>
+        <Text>L'équipe BDS</Text>
+        <Image source={require("../Images/Logo.png")} style={styles.logo} />
+        <Image source={require("../Images/Logo.png")} style={styles.logo} />
+        <Image source={require("../Images/Logo.png")} style={styles.logo} />
+      </View>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    backgroundColor: "#232c53",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   container: {
-    width: "100%",
-    flexDirection: "row",
-    justifyContent: "flex-start",
+    width: '80%'
   },
-  logo: {
-    alignSelf: "center",
-    resizeMode: 'contain',
-    flex: 0.2,
-  },
+  logo:{
+    alignSelf :"center",
+  }, 
 });
 
 
--- Screens/Profil.js
+++ Screens/Profil.js
@@ -1,49 +1,75 @@
-import { StyleSheet, View, TextInput, Button, SafeAreaView, TouchableOpacity, Text, Image } from 'react-native';
+
 import {FontAwesome5, Ionicons} from "@expo/vector-icons";
 import { useLayoutEffect } from 'react';
 import React, { useState, useEffect, useContext } from 'react';
+import { StatusBar } from "expo-status-bar";
+import { StyleSheet, Text, View, FlatList,ScrollView } from "react-native";
+import {
+  Alert,
+  TextInput,
+  Button,
+  TouchableOpacity,
+  Image,
+} from "react-native";
 
+import * as SQLite from "expo-sqlite";
 
 const ITEM_WIDTH = 150;
 
 
 export default function Profil () {  
 
-    // const db = SQLite.openDatabase({name: 'mydb.db', location: 'default'});
+    const db = SQLite.openDatabase({name: 'mydb.db', location: 'default'});
 
     const [Licence, SetLicence] = useState("");
     const [Mail, SetMail] = useState("");
+    const [UserPrenom, setUserPrenom] = useState("");
+    const [UserNom, setUserNom] = useState("");
 
-    // const MAJ = () => {
-    //     db.transaction(txn =>{
-    //         txn.executeSQL(
-    //             "Update table USER where ID_licence"
-    //         )
-    //     })
+     const MAJ = () => {
+         db.transaction(txn =>{
+            txn.executeSQL(
+                 "Update table USER where ID_licence"
+             )
+         })
 
-    // }
+     }
+     const fetchData = (db, setUserPrenom, setUserNom) => {
+        db.transaction((tx) => {
+            tx.executeSql(
+                "SELECT * FROM User WHERE num_Licence = ?",
+                ["1"], // Remplace "1" par la licence de l'utilisateur que tu souhaites afficher
+                (_, resultSet) => {
+                const rows = resultSet.rows;
+                if (rows.length > 0) {
+                    const user = rows.item(0);
+                    setUserPrenom(user.User_prenom);
+                    setUserNom(user.User_nom);
+                }
+                },
+                (_, error) => {
+                console.log("Erreur lors de la requête User :", error);
+                }
+            );
+        });
+    };
+
+
+    useEffect(() => {
+        fetchData();
+    }, []);
+
     return (
-        <View>
+        <View style={styles.container}>
             <View style={{alignContent: 'center', alignItems: 'center', marginTop: 25}}>
                <Image 
                 style={styles.image}
                 source={require('../assets/profil.png')}/> 
             </View>
             <View style={{margin: 25}}>
-                <TextInput 
-                    placeholder="Identifiant : "
-                    value={Licence}
-                    onChangeText={SetLicence}/>
-                <TextInput
-                    placeholder="mail : "
-                    value={Mail}
-                    onChangeText={SetMail} />
-                <TextInput
-                    placeholder="Licence : "
-                    value={Licence}
-                    onChangeText={SetLicence} /> 
+                <Text style={styles.text}>Identifiant: {UserPrenom}</Text>
+                <Text style={styles.text}>Mail: {UserNom}</Text>
             </View>
- 
         </View>
     );
 }
@@ -67,7 +93,7 @@ const styles = StyleSheet.create({
     container: {
         flexDirection: 'column',
         flex: 1,
-        backgroundColor: "#FFF",
+        backgroundColor: "#232c53",
     },
     text: {
         color:"#161924",
